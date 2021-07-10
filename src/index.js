7f1bbdd1430a1b8dc185ca675592c053022532ff
Hung Duong Quang <qhungg289@protonmail.com>
2021-07-10 17:12:33 +0700

    Import modules and assign event listeners

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import "./style.css"
-import todoGenerator from "./todoGenerator"
+import { todosCurrent, todosCompleted, todoMaster, createNewTodo, createNewLabel } from "./app";
+import {
+    formButton,
+    formTitle,
+    formDesc,
+    formDate,
+    formPriority,
+    newLabelText,
+    newLabelButton,
+    appendTodosToList,
+    resetFormValue,
+    setIndexCheckBoxesOnMainList
+} from "./DOM";
+
+formButton.onclick = () => {
+    createNewTodo(
+        todosCurrent,
+        formTitle.value,
+        formDesc.value,
+        formDate.value,
+        formPriority.value
+    )
+    appendTodosToList(formTitle.value, formDesc.value);
+    setIndexCheckBoxesOnMainList();
+    resetFormValue();
+};
+
+newLabelButton.onclick = () => {
+    createNewLabel(newLabelText.value);
+}
