dc1fc52145e160be7c55f84bf4ccd639438955f3
Hung Duong Quang <qhungg289@protonmail.com>
2021-07-17 20:50:22 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,47 @@ import "./style.css"
 import { todoGenerator } from "./todoGenerator"
 import * as DOM from "./DOM";
 
+let defaultTag = [];
+let tagContainer = [defaultTag];
+defaultTag.name = "Default";
+
+// Init render
+DOM.renderTags(tagContainer);
+
+// Create new tag
+function createNewTag(field) {
+    let tag = [];
+    tag.name = field;
+    tagContainer.push(tag);
+    DOM.closeTagModalBtn.click();
+    DOM.renderTags(tagContainer);
+    console.log(tagContainer);
+}
+
+function checkForNameDuplicate(field) {
+    for (let i = 0; i < tagContainer.length; i++) {
+        if (field == tagContainer[i].name) {
+            return true;
+        }
+    }
+}
+
+DOM.createTagBtn.onclick = () => {
+    if (checkForNameDuplicate(DOM.tagNameField.value) == true) {
+        alert("Name already exist!");
+    } else if (DOM.tagNameField.value == "") {
+        alert("Don't leave it empty!");
+    } else {
+        createNewTag(DOM.tagNameField.value);
+        DOM.tagNameField.value = null;
+    }
+}
+
+DOM.tagNameField.addEventListener("keyup", event => {
+    if (event.key !== "Enter") return;
+    DOM.createTagBtn.click();
+    event.preventDefault();
+    DOM.tagNameField.value = null;
+})
+
+export { tagContainer };
